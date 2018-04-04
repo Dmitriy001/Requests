29a0024478ad5e0e767c33193d0181516db28fe8
Dmitriy Vasin <vasindmitriy1992@gmail.com>
2018-04-04 21:11:41 +0300

    before ch

--- index.js
+++ index.js
@@ -6,18 +6,36 @@ function getData(url){
         xhr.open('GET', url, true);
         xhr.send();
         xhr.onload = function () {
-            if (this.readyState == 4 ) { // or this.status == 200
-                resolve(this.response);
+            if (this.readyState == 4) {// or this.status == 200
+                resolve(JSON.parse(this.response));
             } else reject('error');
         };
     })
 }
 
 
+
 getData('https://jsonplaceholder.typicode.com/posts')
-    .then(function(data){
-       console.log(data);})
+    .then(function (data) {
+            var arr = [];
+            for (var i =0; i <= data.length-1; i++) {
+                var url = 'https://jsonplaceholder.typicode.com/posts/' + data[i].id;
+                getData(url)
+                    .then(function (value) { console.log(value) });
+
+            }
+            // Promise.all(arr).then(function(responses){
+            //     responses.forEach(function (value) {
+            //         console.log(value);
+            //     })
+            // });
+        }
+    )
+    .then(function (array) {
+
+    })
     .catch(function (error) {
         alert(error);
     });
+
 
